bac1f62488c975afc0b1aca3a6246ad167b63f5f
laraking <laraking@umich.edu>
2025-06-08 21:32:54 -0400

    Change format of game data

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,15 @@ import Game from './components/Game';
 import mockTilesData from './components/data/mockTileData';
 
 function App() {
+  const tilesData = mockTilesData.flatMap((theme) => theme.words.map((word) => ({
+    word,
+    theme: theme.theme,
+    category: theme.category,
+    colors: theme.colors,
+  })));
   return (
     <div className="App">
-      <Game tilesData={mockTilesData} />
+      <Game tilesData={tilesData} />
     </div>
   );
 }
--- src/components/data/mockTileData.jsx
+++ src/components/data/mockTileData.jsx
@@ -1,20 +1,24 @@
 const mockTilesData = [
-  { word: 'Apple', theme: 'Fruits', colors: ['red', '#000'] },
-  { word: 'Banana', theme: 'Fruits', colors: ['red', '#000'] },
-  { word: 'Cherry', theme: 'Fruits', colors: ['red', '#000'] },
-  { word: 'Date', theme: 'Fruits', colors: ['red', '#000'] },
-  { word: 'Lion', theme: 'Animals', colors: ['blue', '#000'] },
-  { word: 'Tiger', theme: 'Animals', colors: ['blue', '#000'] },
-  { word: 'Elephant', theme: 'Animals', colors: ['blue', '#000'] },
-  { word: 'Giraffe', theme: 'Animals', colors: ['blue', '#000'] },
-  { word: 'Car', theme: 'Vehicles', colors: ['green', '#000'] },
-  { word: 'Bus', theme: 'Vehicles', colors: ['green', '#000'] },
-  { word: 'Bike', theme: 'Vehicles', colors: ['green', '#000'] },
-  { word: 'Truck', theme: 'Vehicles', colors: ['green', '#000'] },
-  { word: 'Pluto', theme: 'Planets', colors: ['yellow', '#000'] },
-  { word: 'Mars', theme: 'Planets', colors: ['yellow', '#000'] },
-  { word: 'Earth', theme: 'Planets', colors: ['yellow', '#000'] },
-  { word: 'Venus', theme: 'Planets', colors: ['yellow', '#000'] },
+  {
+    theme: 'Vehicles',
+    words: ['Car', 'Bus', 'Bike', 'Truck'],
+    category: 1,
+  },
+  {
+    theme: 'Animals',
+    words: ['Lion', 'Tiger', 'Elephant', 'Giraffe'],
+    category: 2,
+  },
+  {
+    theme: 'Fruits',
+    words: ['Apple', 'Banana', 'Cherry', 'Date'],
+    category: 3,
+  },
+  {
+    theme: 'Planets',
+    words: ['Pluto', 'Mars', 'Earth', 'Venus'],
+    category: 4,
+  },
 ];
 
 export default mockTilesData;
